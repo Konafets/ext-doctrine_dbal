52137987a939f271eaf1934119390e2b023ac2fb
Stefano Kowalke <blueduck@gmx.net>
2014-05-10 19:57:40 +0200

    Add the methods new API
    The new API inherits from the old one. So change also the XCLASS to
    the new classes.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,5 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\DatabaseConnection'] = array('className' => 'Konafets\\DoctrineDbal\\Persistence\\Legacy\\DatabaseConnection');
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\PreparedStatement'] = array('className' =>  'Konafets\\DoctrineDbal\\Persistence\\Legacy\\PreparedStatement');
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\DatabaseConnection'] = array('className' => 'Konafets\\DoctrineDbal\\Persistence\\Doctrine\\DatabaseConnection');
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\PreparedStatement'] = array('className' =>  'Konafets\\DoctrineDbal\\Persistence\\Doctrine\\PreparedStatement');
